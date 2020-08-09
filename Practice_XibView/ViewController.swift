4061c2b815d2d57b993d45ed453c2639ee52f778
Miharu Naruse <nars024@gmail.com>
2020-08-10 08:34:08 +0900

    Initial Commit

---
+++ Practice_XibView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice_XibView
+//
+//  Created by MiharuNaruse on 2020/08/10.
+//  Copyright © 2020 m_rn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
