3bf1ec79f6a0f77e5edb8c40e0c925d8c4748b8e
Miharu Naruse <nars024@gmail.com>
2020-08-10 19:59:34 +0900

    Add FirstViewController. Rename from FirstView to XibView again.

---
+++ Practice_XibView/ViewControllers/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  Practice_XibView
+//
+//  Created by MiharuNaruse on 2020/08/10.
+//  Copyright © 2020 m_rn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
+}
